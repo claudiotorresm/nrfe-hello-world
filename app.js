bc56528f28c200b525bc504df0258a26fae55bdd
psvensson <psvensson@gmail.com>
2015-12-10 15:43:54 +0100

    added mustache requirement

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ requirejs.config(
 	baseUrl: 'bower_components',
 	paths:
 	{
-		mustache: 'mustache.js/mustache',
+		Mustache: 'mustache.js/mustache',
 		nrfe: 'nrfe/nrfe',
 		json: 'requirejs-plugins/src/json',
 		text: 'requirejs-plugins/lib/text'
@@ -18,13 +18,18 @@ requirejs.config(
 });
 
 // Start the main app logic.
-requirejs(['nrfe','jquery/dist/jquery','mustache','json!../flows.json', 'json!nrfe-widgets/widgetlist.json'],
-	function(nrfe, $, mustache, definition, widgetlist)
+requirejs(['nrfe','jquery/dist/jquery','Mustache','json!../flows.json', 'json!nrfe-widgets/widgetlist.json'],
+	function(nrfe, $, Mustache, definition, widgetlist)
 	{
-		var generator = new nrfe(widgetlist, function()
-		{
-			generator.render(definition, document.getElementById("content"));
-		});
-
-
+		document.addEventListener(
+			'deviceready',
+			function()
+			{
+				console.log('device ready - now loading the show..');
+				var generator = new nrfe(widgetlist, function()
+				{
+					generator.render(definition, document.getElementById("content"));
+				});
+			},
+			false);
 	});
