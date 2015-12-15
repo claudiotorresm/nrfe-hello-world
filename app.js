fdc8ed0f582ebcf10a3b77d20b3eb7c277853a18
psvensson <psvensson@gmail.com>
2015-12-15 13:13:13 +0100

    started using the browserified nrfe stuff

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-requirejs.config(
-{
-	baseUrl: 'bower_components',
-	paths:
-	{
-		Mustache: 'mustache.js/mustache',
-		nrfe: 'nrfe/nrfe',
-		json: 'requirejs-plugins/src/json',
-		text: 'requirejs-plugins/lib/text'
-	},
-	shim:
-	{
-		'jquery/dist/jquery':
-		{
-			exports: '$'
-		}
-	}
-});
 
-// Start the main app logic.
-requirejs(['nrfe','jquery/dist/jquery','Mustache','json!../flows.json', 'json!nrfe-widgets/widgetlist.json'],
-	function(nrfe, $, Mustache, definition, widgetlist)
+$.ajax({
+	complete: function(flows)
 	{
-		document.addEventListener(
-			'deviceready',
-			function()
-			{
-				console.log('device ready - now loading the show..');
-				var generator = new nrfe(widgetlist, function()
-				{
-					generator.render(definition, document.getElementById("content"));
-				});
-			},
-			false);
-	});
+		console.log('flows loaded..');
+		var target = document.getElementById('content');
+		new nrfe(nrfeWidgets, function(generator)
+		{
+			console.log('gogogo');
+			generator.render(JSON.parse(flows.responseText), target);
+		});
+	},
+	url: "flows.json"
+});
